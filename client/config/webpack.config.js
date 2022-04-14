9fcbedcffae94b5ae9fa8d021de6017d05e6b98a
hwang-jy <dev.im.hwang@gmail.com>
2022-04-14 18:27:36 +0900

    add hotload

--- client/config/webpack.config.js
+++ client/config/webpack.config.js
@@ -401,6 +401,19 @@ module.exports = function (webpackEnv) {
                 and: [/\.(ts|tsx|js|jsx|md|mdx)$/],
               },
             },
+
+            {
+              test: /\.(js|jsx)$/,
+              include: paths.appSrc,
+              loader: require.resolve('babel-loader'),
+              options: {
+                plugins: ['react-hot-loader/babel'],
+                // This is a feature of `babel-loader` for webpack (not Babel itself).
+                // It enables caching results in ./node_modules/.cache/babel-loader/
+                // directory for faster rebuilds.
+                cacheDirectory: true,
+              },
+            },
             // Process application JS with Babel.
             // The preset includes JSX, Flow, TypeScript, and some ESnext features.
             {
